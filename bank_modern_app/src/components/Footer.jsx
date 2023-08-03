aeded4ce465f0b2e27ab510281efe97963a11c5d
Abbas <meerabbas110@gmail.com>
2023-08-03 12:33:33 +0530

    footer with icons

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -41,6 +41,24 @@ const Footer = () => (
         ))}
       </div>
     </div>
+    {/* last part of footer  */}
+    <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+      <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+        2020 HooBank. All Rights Reserved.
+      </p>
+      <div className='flex flex-row md:mt-0 mt-6'>
+        {socialMedia.map((social, index) => (
+          <img
+            src={social.icon}
+            key={social.id}
+            alt='icon'
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== social.length - 1 ? 'mr-6' : 'mr-0'
+            } `}
+          />
+        ))}
+      </div>
+    </div>
   </section>
 );
 
