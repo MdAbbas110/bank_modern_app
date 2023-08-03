5cac40cf05da9b676274b532aca8f798359a790a
Abbas <meerabbas110@gmail.com>
2023-08-03 12:46:39 +0530

    minor fixes in hero index

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -3,7 +3,10 @@ import { discount, robot } from '../assets';
 import GetStarted from './GetStarted';
 
 const Hero = () => (
-  <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+  <section
+    id='home'
+    className={`flex md:flex-row flex-col xl:ml-28 ${styles.paddingY}`}
+  >
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
